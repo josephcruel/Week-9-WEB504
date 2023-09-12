71f979d42ba0c2de6243812c91873f982f3d4563
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-09-12 12:25:30 +1200

    Used GetElementsByTagName

--- dom-events/index.js
+++ dom-events/index.js
@@ -1,16 +1,23 @@
 //NOTE
 
+let btn = document.getElementById('btnCount')
+btn.addEventListener('click',() => {
+    let hTwoHeader = document.getElementsByTagName('h2')
+    alert(`The number of H2 tags are: ${hTwoHeader.length}`)
+})
+
+
 // const p = document.getElementById('message'); // This would return the value "null"
 // console.log(p);
 
-let btn = document.getElementById('btnRadio')
-let output = document.getElementById('output')
+// let btn = document.getElementById('btnRadio')
+// let output = document.getElementById('output')
 
-btn.addEventListener('click', () => {
-    let languages = document.getElementsByName('language')
-    languages.forEach((language) => {
-        if (language.checked) {
-            output.innerText = `You code with : ${language.value}`
-        }
-    })
-})
+// btn.addEventListener('click', () => {
+//     let languages = document.getElementsByName('language')
+//     languages.forEach((language) => {
+//         if (language.checked) {
+//             output.innerText = `You code with : ${language.value}`
+//         }
+//     })
+// })
