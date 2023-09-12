a5da831598780ab3dc0959371df6c4667144cb6c
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-09-12 12:50:30 +1200

    empty array, mapping, not too sure

--- dom-events/index.js
+++ dom-events/index.js
@@ -1,14 +1,29 @@
 //NOTE
 
-let btn = document.getElementById('btnCount')
-btn.addEventListener('click',() => {
-    let hTwoHeader = document.getElementsByTagName('h2')
-    alert(`The number of H2 tags are: ${hTwoHeader.length}`)
-})
+//all declarations by using 'let'
+//targeting the ul class tag 'menu' by getElementByClassId
+let menu = document.getElementByClassId('menu')
+//selecting the list class tag 'item' by using getElementByClassName
+let items = menu.getElementByClassName('item')
+
+
+let data = [].map.call(items, item => item.textContent) // ['JavaScript', 'CSS', 'HTML' , 'TypeScript']
+
+
+
+//NOTE - getElementsByTagName
+
+// let btn = document.getElementById('btnCount')
+// btn.addEventListener('click',() => {
+//     let hTwoHeader = document.getElementsByTagName('h2')
+//     alert(`The number of H2 tags are: ${hTwoHeader.length}`)
+// })
 
 
 // const p = document.getElementById('message'); // This would return the value "null"
 // console.log(p);
+
+//NOTE - document.getElementsByName
 
 // let btn = document.getElementById('btnRadio')
 // let output = document.getElementById('output')
